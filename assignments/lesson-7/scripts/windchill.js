0922d80e8273fb5aac226a660edee0da3cb57b5b
dsantosmt <mot16005@byui.edu>
2018-06-09 22:24:27 -0400

    lastmpre

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,4 +1,3 @@
-function wind(){
 var HighTemp = parseInt(document.getElementById("high-temp").innerHTML);
 var LowTemp = parseInt(document.getElementById("low-temp").innerHTML);
 var s = parseInt(document.getElementById("wind-speed").innerHTML);
@@ -6,5 +5,5 @@ var t = (HighTemp + LowTemp) / 2;
 var s_pow = Math.pow(s,0.16);
 var windchill = 35.74 + (0.6215 * t) - (35.75 * s_pow) + (0.4275 * t * s_pow);
 document.getElementById("wind-chill").innerHTML = Math.round(windchill);
-}
-wind();
+
+
